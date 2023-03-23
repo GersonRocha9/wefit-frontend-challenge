c1f8b7fa5b8e71aac9fee46f1fc6cc25fd080cd0
Gerson Rocha <gersonrocha9@gmail.com>
2023-03-23 10:49:06 -0300

    feat: header component done ✅

---
+++ wefit-react-challenge/src/components/Header/index.tsx
@@ -0,0 +1,51 @@
+import {
+  CartContainer,
+  CartInfoContainer,
+  ContentContainer,
+  HeaderContainer,
+  Image,
+} from './styles'
+
+import bagIcon from '../../assets/bagIcon.svg'
+import { Typography } from '../Typography'
+// import { HTMLAttributes } from "react";
+import { useNavigate } from 'react-router-dom'
+
+// import { ICartItem } from "../../@types/coffee";
+
+// interface HeaderProps extends HTMLAttributes<HTMLDivElement> {
+//   cartItems: ICartItem[];
+// }
+
+export const Header = () => {
+  // ROUTER
+  const navigate = useNavigate()
+
+  return (
+    <HeaderContainer>
+      <ContentContainer>
+        <Typography
+          size={20}
+          weight={700}
+          onClick={() => navigate('/')}
+          style={{ cursor: 'pointer' }}
+        >
+          WeMovies
+        </Typography>
+
+        <CartContainer onClick={() => navigate('/cart')}>
+          <CartInfoContainer>
+            <Typography size={14} weight={600} className="cart-title">
+              Meu Carrinho
+            </Typography>
+
+            <Typography size={12} weight={600} color="#999">
+              0 itens
+            </Typography>
+          </CartInfoContainer>
+          <Image src={bagIcon} alt="Bag Icon" />
+        </CartContainer>
+      </ContentContainer>
+    </HeaderContainer>
+  )
+}
---
+++ wefit-react-challenge/src/components/Header/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+
+  img {
+    width: 85px;
+    cursor: pointer;
+  }
+`
+
+export const ContentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 24.5px 10px;
+`
+
+export const CartContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  alignitems: center;
+  cursor: pointer;
+`
+
+export const CartInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding: 0px;
+
+  @media (max-width: 768px) {
+    .cart-title {
+      display: none;
+    }
+  }
+`
+
+export const Image = styled.img`
+  width: 29.32px !important;
+  height: 25.31px;
+`
