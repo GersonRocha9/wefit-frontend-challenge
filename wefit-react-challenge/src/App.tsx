91e5b8b542195d54fa22fef7bb9490605b9b04cd
Gerson Rocha <gersonrocha9@gmail.com>
2023-03-27 20:13:30 -0300

    feat: implement default exports

--- wefit-react-challenge/src/App.tsx
+++ wefit-react-challenge/src/App.tsx
@@ -1,8 +1,8 @@
+import { GlobalStyles, defaultTheme } from './styles'
+
 import { ThemeProvider } from 'styled-components'
 import { MovieProvider } from './contexts'
 import { AppRoutes } from './routes'
-import { GlobalStyles } from './styles/globalStyles'
-import { defaultTheme } from './styles/themes'
 
 export const App = () => {
   return (
